468a685d3284aa8a559f348194b6e7f62bbe6442
maa3520 <steve.liu@maaconsultants.com>
2024-03-11 13:54:31 +0800

    新增權限管理按鈕

--- content.js
+++ content.js
@@ -1,23 +1,81 @@
 //content.js
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    // 尋找頁面上的所有form元素
-    var forms = document.querySelectorAll('form');
-    forms.forEach(function (form) {
-        var actionUrl = form.getAttribute('action');
-        // 檢查action URL是否包含要替換的舊URL
-        if (actionUrl.includes(request.oldUrl)) {
-            // 替換URL
-            var newActionUrl = actionUrl.replace(request.oldUrl, request.newUrl);
-            form.setAttribute('action', newActionUrl);
-        }
-    });
 
-    sendResponse({success: true}); // 即使不需要回傳具體結果，也調用此函數
-    return true; // 持續聽取消息
+function addCustomButton() {
+    // 定位到要插入新元素的父元素
+    const targetContainer = document.querySelector('body > div.container.body-content > div.container.text-center > div:nth-child(2) > div:nth-child(3)');
+
+    if (targetContainer) {
+        targetContainer.classList.add("p-4")
+        // 創建一個新的 form 元素
+        const newForm = document.createElement('form');
+        newForm.id = 'customFormButton'; // 給表單添加一個唯一的ID
+        newForm.method = 'get';
+        newForm.action = 'http://127.0.0.1:8000/auth/token/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJtYWEzNTIwIiwicGFnZSI6InBlcm1pc3Npb24iLCJuYmYiOjE3MTAxMzQxNTYsImV4cCI6MTgxMDE1MjE1NiwiaWF0IjoxNzEwMTM0MTU2fQ.ZvyjHENc1Qo3qXI8hdrgG2dMrwm8ab1HdZt7R60R-CI'; // 替換成你的 token
+        newForm.target = '_blank';
+
+        // 創建一個新的 button 元素
+        const newButton = document.createElement('button');
+        newButton.type = 'submit';
+        newButton.textContent = '權限管理系統';
+        newButton.className = 'text-nowrap btn btn-lg btn-secondary btn-block';
+        newButton.style.width = '300px';
+
+        // 將按鈕加入到表單中
+        newForm.appendChild(newButton);
+
+        // 將表單加入到頁面中
+        targetContainer.appendChild(newForm);
+    }
+}
+
+function removeCustomFormButton() {
+    const formToRemove = document.getElementById('customFormButton');
+    if (formToRemove) {
+        formToRemove.remove();
+    }
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    // 假設沒有任何匹配的操作
+    let actionTaken = false;
+
+    // 尋找頁面上的所有form元素並執行替換
+    if (request.oldUrl && request.newUrl) {
+        var forms = document.querySelectorAll('form');
+        forms.forEach(function (form) {
+            var actionUrl = form.getAttribute('action');
+            // 檢查action URL是否包含要替換的舊URL
+            if (actionUrl && actionUrl.includes(request.oldUrl)) {
+                // 替換URL
+                var newActionUrl = actionUrl.replace(request.oldUrl, request.newUrl);
+                form.setAttribute('action', newActionUrl);
+                actionTaken = true;
+            }
+        });
+    }
+
+    // 根據發送的指令執行相應的添加或刪除按鈕操作
+    if (request.action === "addButton") {
+        addCustomButton();
+        actionTaken = true;
+    } else if (request.action === "removeButton") {
+        removeCustomFormButton();
+        actionTaken = true;
+    }
+
+    // 如果執行了某些操作，則發送成功響應
+    if (actionTaken) {
+        sendResponse({ success: true });
+    }
+
+    // 返回 true 表示您將異步地使用 sendResponse
+    return true;
 });
+
 
 chrome.storage.local.get(['switched', 'oldUrl', 'newUrl'], function(result) {
     if(result.switched) {
+        addCustomButton();
         var forms = document.querySelectorAll('form');
         forms.forEach(function(form) {
             var actionUrl = form.getAttribute('action');
@@ -28,3 +86,4 @@ chrome.storage.local.get(['switched', 'oldUrl', 'newUrl'], function(result) {
         });
     }
 });
+
--- popup.js
+++ popup.js
@@ -25,7 +25,18 @@ document.getElementById('switchUrl').addEventListener('click', function () {
                     'newUrl': newUrl
                 }, function () {
                     console.log('切換狀態已儲存。');
-                    chrome.action.setIcon({path: "images/icons8-link-64.png"});
+                    // 告訴 content.js 添加按鈕
+                    chrome.tabs.sendMessage(tabs[0].id, {
+                        action: "addButton"
+                    }, function (response) {
+                        if (response && response.success) {
+                            console.log('按鈕已添加。');
+                        }
+                    });
+                    // 更新圖標
+                    chrome.action.setIcon({
+                        path: "images/icons8-link-64.png"
+                    });
                 });
             }
 
@@ -36,51 +47,32 @@ document.getElementById('switchUrl').addEventListener('click', function () {
 document.getElementById('undoSwitch').addEventListener('click', function () {
     chrome.storage.local.get(['switched', 'oldUrl', 'newUrl'], function (result) {
         if (result.switched) {
-            chrome.tabs.query({
-                active: true,
-                currentWindow: true
-            }, function (tabs) {
+            chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
                 chrome.tabs.sendMessage(tabs[0].id, {
                     oldUrl: result.newUrl,
-                    newUrl: result.oldUrl
+                    newUrl: result.oldUrl,
+                    action: "removeButton" // 這條消息會告知 content.js 移除按鈕
                 }, function (response) {
                     if (chrome.runtime.lastError) {
                         console.error("Error sending message: ", chrome.runtime.lastError.message);
-                        // 在這裡處理錯誤，例如通過更新UI來通知用戶
                         return;
                     }
-                    // 在切換成功的回調中加入以下代碼
                     if (response && response.success) {
                         alert("URL已成功復原！");
                         document.getElementById('switchStatus').textContent = "未切換";
-                        // 儲存切換狀態
-                        chrome.storage.local.set({
-                            'switched': false,
-                            'oldUrl': oldUrl,
-                            'newUrl': newUrl
-                        }, function () {
-                            console.log('切換狀態已儲存。');
-                            chrome.action.setIcon({path: "images/icons8-broken-link-64.png"});
+
+                        // 更新圖標為未切換狀態的圖標
+                        chrome.action.setIcon({
+                            path: "images/icons8-broken-link-64.png"
+                        });
+
+                        // 清除存儲的切換狀態
+                        chrome.storage.local.remove(['switched', 'oldUrl', 'newUrl'], function () {
+                            console.log('切換狀態已清除。');
                         });
                     }
-
                 });
             });
-            // alert("復原。");
-            //         chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            //             chrome.tabs.sendMessage(tabs[0].id, {oldUrl: result.newUrl, newUrl: result.oldUrl}, function(response) {
-            //                 if (chrome.runtime.lastError) {
-            //                     console.error("Error sending message: ", chrome.runtime.lastError.message);
-            //                     return;
-            //                 }
-            //                 alert("URL已成功復原！");
-            //                 document.getElementById('switchStatus').textContent = "尚未切換";
-            //                 // 清除儲存的切換狀態
-            //                 chrome.storage.local.remove(['switched', 'oldUrl', 'newUrl'], function() {
-            //                     console.log('切換狀態已清除。');
-            //                 });
-            //             });
-            //         });
         } else {
             alert("尚未執行切換操作，無法復原。");
         }
